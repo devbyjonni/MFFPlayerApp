ca9a41961ee2f97eff58513257dabacbb2fb61b1
Jonni Åkesson <devbyjonni@gmail.com>
2025-02-07 14:29:52 +0100

    Initial commit

---
+++ MFFPlayerApp/App/MFFPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MFFPlayerAppApp.swift
+//  MFFPlayerApp
+//
+//  Created by Jonni Akesson on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct MFFPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
