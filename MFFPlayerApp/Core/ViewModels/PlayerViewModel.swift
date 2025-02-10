cd790448fdf992737e75187fc5b8dcc223ed5efb
Jonni Åkesson <devbyjonni@gmail.com>
2025-02-10 17:10:50 +0100

    Added loading indicator to PlayerListView

--- MFFPlayerApp/Core/ViewModels/PlayerViewModel.swift
+++ MFFPlayerApp/Core/ViewModels/PlayerViewModel.swift
@@ -1,6 +1,5 @@
 import Foundation
 import Observation
-import SwiftData
 
 @MainActor
 @Observable
--- MFFPlayerApp/Core/Views/PlayerListView.swift
+++ MFFPlayerApp/Core/Views/PlayerListView.swift
@@ -14,22 +14,29 @@ struct PlayerListView: View {
     
     var body: some View {
         NavigationView {
-            List(storedPlayers) { player in
-                HStack {
-                    AsyncImage(url: URL(string: player.image)) { image in
-                        image.resizable()
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 50, height: 50)
-                    .clipShape(Circle())
-                    
-                    VStack(alignment: .leading) {
-                        Text(player.name)
-                            .font(.headline)
-                        Text("#\(player.number)")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
+            VStack {
+                if viewModel.isLoading {
+                    ProgressView("Loading players...")
+                        .padding()
+                } else {
+                    List(storedPlayers) { player in
+                        HStack {
+                            AsyncImage(url: URL(string: player.image)) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text(player.name)
+                                    .font(.headline)
+                                Text("#\(player.number)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
                     }
                 }
             }
